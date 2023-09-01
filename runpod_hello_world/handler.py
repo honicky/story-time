6a4fbb896ccea6406671a0d2ca69a4733681515f
RJ Honicky <rj@cred.bet>
2023-09-01 12:14:50 -0700

    testing runpod

---
+++ runpod_hello_world/handler.py
@@ -0,0 +1,24 @@
+import runpod
+import os
+import time
+
+sleep_time = int(os.environ.get('SLEEP_TIME', 3))
+
+# load lamma-2-70B-instruct from huggingface
+
+
+def handler(event):
+    print(event)
+    time_slept = 0
+    while time_slept < sleep_time:
+        print("working, I promise")
+        time_slept += 1
+        time.sleep(1)
+    # do the things
+
+    return "Hello World"
+
+
+runpod.serverless.start({
+    "handler": handler
+})
