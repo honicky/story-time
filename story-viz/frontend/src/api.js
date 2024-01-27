5314d9f6dd571e21aca2625ea882ee1aa082876c
honicky <honicky@gmail.com>
2024-01-27 09:52:01 -0800

    Remove commented code from story-viz api.js

--- story-viz/frontend/src/api.js
+++ story-viz/frontend/src/api.js
@@ -1,19 +1,6 @@
 import axios from 'axios';
 
-// const authorizationHeader = import.meta.env.VITE_AUTHORIZATION;
-// const headers = {"Authorization": `Basic ${authorizationHeader}`};
-// const referrerPolicy = "origin";
-// const requestParams = { headers, referrerPolicy };
-// const requestParams = { header };
-
 const defaultHeaders = {};
-
-// const baseApiUrls = {
-//   dev: "https://81rq7.apps.beam.cloud",
-//   staging: "https://ri8tb.apps.beam.cloud",
-//   production: "https://2oihh.apps.beam.cloud",
-// };
-// const baseApiUrl = baseApiUrls[import.meta.env.VITE_STAGE || "dev"] ;
 const baseApiUrl = "/api"
 
 const getHeadersWithAuthorization = (token) => {
